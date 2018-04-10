ae171c6eeee3bfa0b1dbe1f1bda05981c26b070a
Michael <michael@garstka.org>
2018-04-10 16:40:39 +0100

    Working LMI stability control problem

--- Code/Helper.jl
+++ Code/Helper.jl
@@ -1,6 +1,6 @@
 module Helper
 
-export generatePosDefMatrix
+export generatePosDefMatrix, transposeVectorized
 
   # generate a random pos def matrix with eigenvalues between 0.1 and 2
   function generatePosDefMatrix(n::Int64,rng)
@@ -12,4 +12,29 @@ export generatePosDefMatrix
       X = 0.5*(X+X')
       return X
   end
+
+  # gives you the position of Aij in vec(A) where A in R^mxn
+  function vecPos(m,n,i,j)
+    ((i > m) || (j > n)) && error("Your indizes are outside the matrix dimension.")
+    return Int((j-1)*m+i)
+  end
+
+  # permutes the cols of matrix A, s.t. A*vec(B) == A[:,p]*vec(B'), with B in R^{m_b x n_b}
+  function transposeVectorized(A,mb,nb)
+    # size of A
+    m,n = size(A)
+
+    # find permutation vector p
+    k = 0
+    p = zeros(Int64,n)
+    for j=1:nb, i=1:mb
+      k += 1
+      p[vecPos(nb,mb,j,i)] = k
+    end
+    #println("Permutation vector: p=$(p)")
+    # permute cols of A
+    return A[:,p]
+
+  end
+
 end #MODULE
--- Code/generateHinfController.jl
+++ Code/generateHinfController.jl
@@ -3,10 +3,9 @@
 
 
 workspace()
-using JLD, JuMP, Mosek
+include("./Helper.jl")
 
-
-
+using JuMP, Mosek
 
 rng = MersenneTwister(12345)
 dirPath = "./TestProblems/RobustControl/"
@@ -43,17 +42,29 @@ nn = 25
   Dzu = zeros(nz,nu)
 
   # LMI 1): Lyapunov stability: PA'+AP+Y'Bu'+BuY < 0 (NOTE STRICT INEQUALITY)
+  AP  = kron(eye(n),A)
+  PAt = kron(A,eye(n))
+  BuY = kron(eye(n),Bu)
+  YtBut = kron(Bu,eye(n))
+  # turn YtBut*vec(Y') into YtBut* vec(Y) by permuting cols of YtBut
+  YtBut = transposeVectorized(YtBut,n,nu) #provide dimension of Y'
 
+  Aa = [AP+PAt BuY+YtBut]
+  ba = zeros(size(Aa,1),1)
+  Pa = spzeros(size(Aa,2),size(Aa,2))
+  qa = spzeros(size(Aa,2),1)
 
   # # solve accurately once with mosek
   model = Model(solver=MosekSolver())
   @variable(model, P[1:n,1:n], SDP)
   @variable(model, Y[1:nu,1:n])
-  @variable(model, g)
-  @objective(model, Min , g)
-  LMI = [A*P+P*A'+Bu*Y+Y'*Bu' Bw P*Cz'+Y'*Dzu';
-          Bw' -g*eye(nw) Dzw';
-          Cz*P+Dzu*Y Dzw -g*eye(nz)]
+  @objective(model,Min,0)
+  # @variable(model, g)
+  # @objective(model, Min , g)
+  # LMI = [A*P+P*A'+Bu*Y+Y'*Bu' Bw P*Cz'+Y'*Dzu';
+  #         Bw' -g*eye(nw) Dzw';
+  #         Cz*P+Dzu*Y Dzw -g*eye(nz)]
+  LMI = A*P+P*A'+Bu*Y+Y'*Bu'
   @SDconstraint(model, LMI <= 0)
   status = JuMP.solve(model)
 
@@ -65,8 +76,24 @@ nn = 25
   # compute closed loop system matrix Ac
   Ac = A+Bu*F
   eig(Ac)
-  # compute Hinf gain
 
+  # compute once gain in other fromat
+  # # solve accurately once with mosek
+  model2 = Model(solver=MosekSolver())
+  @variable(model2, S[1:n,1:n], SDP)
+  @variable(model2, P[1:n,1:n], SDP)
+  @variable(model2, Y[1:nu,1:n])
+  x = [vec(P);vec(Y)]
+  s = vec(S)
+  @constraint(model2,Aa*x+s.==ba)
+
+  status = JuMP.solve(model2)
+
+  objTrue = getobjectivevalue(model2)
+  P2 = reshape(getvalue(x)[1:n^2],n,n)
+  Y2 = reshape(getvalue(x)[n^2+1:end],nu,n)
+  F2 = Y2/P2
+  Ac2 = A+Bu*F
 
   # println("Objective value: ",objTrue)
   # println("Corrected objective value: ", objCorrected)
